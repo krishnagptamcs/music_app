2725b39988284e71aab0c3d9eb2ebc27c537771f
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-02-22 01:59:18 +0530

    Ading speend insigths and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +24,8 @@ export default function RootLayout({
           <Navbar />
         </div>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
