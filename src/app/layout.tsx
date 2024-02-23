a377d52c7c99e482269a2555a7f7d4612126de99
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-02-23 10:56:12 +0530

    script tag added in page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
@@ -18,15 +19,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
+    <>
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.$crisp=[];
+              window.CRISP_WEBSITE_ID="fb580669-652e-4990-b879-0ae180270fb9";
+              (function() {
+                var d=document;
+                var s=d.createElement("script");
+                s.src="https://client.crisp.chat/l.js";
+                s.async=1;
+                d.getElementsByTagName("head")[0].appendChild(s);
+              })();
+            `,
+          }}
+        />
+      </Head>
+      <div className={inter.className}>
         <div className="relative w-full flex items-center justify-center ">
           <Navbar />
         </div>
         {children}
         <Analytics />
         <SpeedInsights />
-      </body>
-    </html>
+      </div>
+    </>
   );
 }
