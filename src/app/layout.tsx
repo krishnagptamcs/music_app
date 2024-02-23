3e05f79f0494a783e8b164420d1c01382ca1c60e
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-02-23 11:24:52 +0530

    remove third party script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,33 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.$crisp=[];
-              window.CRISP_WEBSITE_ID="fb580669-652e-4990-b879-0ae180270fb9";
-              (function() {
-                var d=document;
-                var s=d.createElement("script");
-                s.src="https://client.crisp.chat/l.js";
-                s.async=1;
-                d.getElementsByTagName("head")[0].appendChild(s);
-              })();
-            `,
-          }}
-        />
-      </Head>
-      <div className={inter.className}>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
         <div className="relative w-full flex items-center justify-center ">
           <Navbar />
         </div>
         {children}
         <Analytics />
         <SpeedInsights />
-      </div>
-    </>
+      </body>
+    </html>
   );
 }
